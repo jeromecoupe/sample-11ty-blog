29e61b09e85479a3d862316866c69919ae12f2dd
jérôme coupé <jercoupe@gmail.com>
2019-11-26 13:44:10 +0100

    change contact

--- src/_data/pagehome.js
+++ src/_data/pagehome.js
@@ -1,5 +1,5 @@
 module.exports = {
   title: "Gentse blogging",
   intro:
-    "This is our little corner of the Internet, where we want to share our passion  for the city of Gent. Whether you are visiting Belgium or live in the country, we think our city deserves a visit."
+    "This is our little corner of the Internet, where we want to communicate our passion  for the city of Gent. Whether you are visiting Belgium or live in the country, we think our city deserves a visit."
 };
